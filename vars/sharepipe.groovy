d53af1bc2c343071eaffaf18e319cf1f7d2fe70e
ganesh-redy <ganeshreddy7610@gmail.com>
2025-06-23 11:15:47 +0530

    Update sharepipe.groovy

--- vars/sharepipe.groovy
+++ vars/sharepipe.groovy
@@ -1,31 +1,35 @@
 def call(String name, String image) {
-  withEnv([
-    "NAME=${name}",
-    "DOCKER_IMAGE=${image}"
-  ]) {
-    stage("Google Cloud Login") {
-      sh '''
-        gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
-      '''
-    }
+    pipeline {
+        agent any
 
-    stage("Docker Hub Login") {
-      withCredentials([usernamePassword(
-        credentialsId: 'docker-hub',
-        usernameVariable: 'DOCKER_USER',
-        passwordVariable: 'DOCKER_PASS'
-      )]) {
-        sh '''
-          echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-        '''
-      }
-    }
+        environment {
+            NAME = "${name}"
+            DOCKER_IMAGE = "${image}"
+        }
 
-    stage("Build and Push Docker Image") {
-      sh '''
-        docker build -t $NAME/$DOCKER_IMAGE:$BUILD_NUMBER .
-        docker push $NAME/$DOCKER_IMAGE:$BUILD_NUMBER
-      '''
+        stages {
+            stage("Docker Hub Login") {
+                steps {
+                    withCredentials([usernamePassword(
+                        credentialsId: 'docker-hub',
+                        usernameVariable: 'DOCKER_USER',
+                        passwordVariable: 'DOCKER_PASS'
+                    )]) {
+                        sh '''
+                            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                        '''
+                    }
+                }
+            }
+
+            stage("Build and Push Docker Image") {
+                steps {
+                    sh '''
+                        docker build -t $NAME/$DOCKER_IMAGE:$BUILD_NUMBER .
+                        docker push $NAME/$DOCKER_IMAGE:$BUILD_NUMBER
+                    '''
+                }
+            }
+        }
     }
-  }
 }
